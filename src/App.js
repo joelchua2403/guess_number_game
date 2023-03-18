fd993ad8cb036f304f4e30a0e713d2457e491359
Joel Chua <joel.chua.mingyi@gmail.com>
2023-03-18 17:54:18 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+const getRandomNumber = () => {
+  const randomNumber = (Math.floor(Math.random() * 10));
+  console.log(randomNumber)
+  return randomNumber;
+};
+
+const getMessage = (guess, randomNumber) => {
+  if (guess === randomNumber) {
+   return ("You guessed the number!");
+ } else if (guess > randomNumber) {
+   return ("Your guess is too high!");
+ } else if (guess < randomNumber) {
+   return ("Your guess is too low!");
+ }
+ 
+ }
+
 function App() {
+  const [guess, setGuess] = useState('');
+  const [randomNumber, setRandomNumber] = useState(()=> getRandomNumber());
+  const [message, setMessage] = useState('');
+  const [guesses, setGuesses] = useState([]);
+
+  
+
+
+  const checkGuess = () => {
+    setMessage(getMessage(guess, randomNumber));
+    handleGuesses();
+  }
+
+
+
+  const reset = () => {
+    setRandomNumber(getRandomNumber());
+    setMessage('');
+  }
+
+
+  const handleGuesses = () => {
+    setGuesses([...guesses, guess]);
+  }
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Guess the number!</h1>
+    <p>Guess a number between 1 and 10</p>
+    <input type='number' value={guess} onChange={(e) => setGuess(+e.target.value)}/>
+    
+    { message !== 'You guessed the number!' ? (<button onClick={checkGuess}>Guess</button>) : (<button onClick={reset}>Reset</button>) }
+    <p>{message}</p>
+    <h2>Guesses:</h2>
+    {guesses.map((guess)=> {
+      return (
+      <li>
+        {guess}
+      </li>
+    )})}
+
     </div>
   );
 }
